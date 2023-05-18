a72f1e4bf96ac8eaccd32181d0c8aeae0f872cb8
melanch0lic <kudza76@mail.ru>
2023-05-18 13:05:58 +0300

    flutter_map add  selected location weather

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:weather_app/models/weather_response.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,13 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void getCurrentWeather() async {
+  void getCurrentWeather(LatLng point) async {
     setState(() {
       _isLoading = true;
     });
 
     var url = Uri.parse(
-        'http://api.weatherapi.com/v1/current.json?q=40.7143,-74.006&lang=ru&key=46d58dd98dfe476285563025231605');
+        'http://api.weatherapi.com/v1/current.json?q=${point.latitude},${point.longitude}&lang=ru&key=46d58dd98dfe476285563025231605');
     var response = await http.get(url);
     var decodedRepsonse = WeatherResponse.fromJson(jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>);
     setState(() {
@@ -31,11 +33,12 @@ class _HomePageState extends State<HomePage> {
   }
 
   bool _isLoading = false;
+  LatLng? _currentPoint;
+  bool _isEditingMode = true;
   WeatherResponse? _currentWeather;
 
   @override
   void initState() {
-    getCurrentWeather();
     super.initState();
   }
 
@@ -43,23 +46,101 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Weather App')),
-      body: Center(
-          child: _isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Transform.scale(
-                        scale: 1.8, child: Image.network('https:${_currentWeather!.current.condition.icon}')),
-                    const SizedBox(height: 20),
-                    Text(_currentWeather!.current.condition.text),
-                    const SizedBox(height: 20),
-                    Text('Температура ${_currentWeather!.current.tempCelsies} °C'),
-                    const SizedBox(height: 20),
-                    Text('Локация ${_currentWeather!.location.name}, ${_currentWeather!.location.region}')
-                  ],
-                )),
+      body: _isEditingMode
+          ? Column(
+              children: [
+                Expanded(
+                  child: FlutterMap(
+                    nonRotatedChildren: [
+                      if (_currentPoint == null)
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.5), borderRadius: BorderRadius.circular(15)),
+                              child: const Text(
+                                'Выберите локацию',
+                                style: TextStyle(color: Colors.white, fontSize: 24),
+                              ),
+                            ),
+                          ),
+                        ),
+                      if (_currentPoint != null)
+                        Align(
+                            alignment: Alignment.bottomRight,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      getCurrentWeather(_currentPoint!);
+                                      _isEditingMode = false;
+                                      _currentPoint = null;
+                                    });
+                                  },
+                                  child: const Text('Получить погоду по данной локации')),
+                            ))
+                    ],
+                    options: MapOptions(
+                      keepAlive: true,
+                      center: LatLng(43.0367, 44.6678),
+                      interactiveFlags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
+                      zoom: 3,
+                      onTap: (tapPosition, point) {
+                        setState(() {
+                          _currentPoint = point;
+                        });
+                      },
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate: 'https://tile.openstreetmap.de/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                      if (_currentPoint != null)
+                        MarkerLayer(
+                          markers: [
+                            Marker(
+                                point: _currentPoint!,
+                                builder: (context) => const Icon(
+                                      Icons.location_pin,
+                                      color: Colors.purple,
+                                    ))
+                          ],
+                        )
+                    ],
+                  ),
+                )
+              ],
+            )
+          : Center(
+              child: _isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Transform.scale(
+                            scale: 1.8, child: Image.network('https:${_currentWeather!.current.condition.icon}')),
+                        const SizedBox(height: 20),
+                        Text(_currentWeather!.current.condition.text),
+                        const SizedBox(height: 20),
+                        Text('Температура ${_currentWeather!.current.tempCelsies} °C'),
+                        const SizedBox(height: 20),
+                        Text('Локация ${_currentWeather!.location.name}, ${_currentWeather!.location.region}'),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _isEditingMode = true;
+                              });
+                            },
+                            child: const Text('Изменить локацию'))
+                      ],
+                    )),
     );
   }
 }
